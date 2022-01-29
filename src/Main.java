e1697e4410e5ec34b05c4643ed924e8993a385ec
rafet <83957086+Rafetdur@users.noreply.github.com>
2022-01-29 22:43:01 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,26 @@
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
-        String mesaj = "Bugun Hava cok Guzel.";
+        String mesaj = "          Bugun Hava cok Guzel.          ";
         System.out.println(mesaj);
-        System.out.println("Eleman Sayisi: " + mesaj.length());
+         System.out.println("Eleman Sayisi: " + mesaj.length());
         System.out.println("5. ELeman" + mesaj.charAt(4));
         System.out.println(mesaj.concat("Yasasin!"));
         System.out.println(mesaj.startsWith("A"));
         System.out.println(mesaj.endsWith("."));
         System.out.println(mesaj.indexOf("uz"));
         System.out.println(mesaj.lastIndexOf("a"));
+        String yenimesaj = mesaj.replace(' ', '-');
+        System.out.println(yenimesaj);
+        System.out.println(mesaj.substring(2,5));
+        for (String kelime : mesaj.split( " ")) {
+            System.out.println(kelime);
+
+        }
+        System.out.println(mesaj.toLowerCase());
+        System.out.println(mesaj.toUpperCase());
+        System.out.println((mesaj.trim())); //bosluklari siler
     }
 }
